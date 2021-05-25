8594ced4bc306c78f957d173e7966d3a6e3b409f
Thomas Pollick <thomascpollick@gmail.com>
2021-05-25 07:36:24 -0500

    new stuff

--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -6,15 +6,30 @@ import ReactCardFlip from 'react-card-flip';
 
 const Flashcards = () => {
     const [ isFlipped, setIsFlipped ] = useState(true)
+    const [ flipClass, setFlipClass ] = useState('')
+    const [ cardSide, setCardSide ] = useState('front')
     const handleClick = () => {
         setIsFlipped(!isFlipped)
+
+        if(cardSide !== 'front') {
+            setFlipClass('')
+            setCardSide('front')
+        }
+        else {
+            setFlipClass('flip-horizontal-bottom')
+            setCardSide('back')
+        }
     }
     return (
         <div>
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-                <CardFront handleClick={handleClick} />
-                <CardBack handleClick={handleClick} />
-            </ReactCardFlip>
+            <div onClick={() => handleClick()} className={flipClass}>
+                {cardSide === 'front' ? <CardFront handleClick={handleClick} />
+                 : <CardBack handleClick={handleClick} /> }
+            </div>
+            {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical"> */}
+                {/* <CardFront handleClick={handleClick} /> */}
+                {/* <CardBack handleClick={handleClick} /> */}
+            {/* </ReactCardFlip> */}
         </div>
     )
 }
